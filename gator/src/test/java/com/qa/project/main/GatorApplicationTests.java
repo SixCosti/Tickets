cab7e9d009dc0a701739b62202a505af4b7abac1
Costi <sixcosti@yahoo.com>
2021-11-25 00:32:32 +0000

    ghgh

---
+++ gator/src/test/java/com/qa/project/main/GatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.project.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
