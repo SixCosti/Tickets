c5e5555169997c696ec2d6ca7db97fdf38e20e1f
Costi <sixcosti@yahoo.com>
2021-11-25 00:29:21 +0000

    hi

---
+++ gator/src/main/java/com/qa/project/main/GatorApplication.java
@@ -0,0 +1,13 @@
+package com.qa.project.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatorApplication.class, args);
+	}
+
+}
